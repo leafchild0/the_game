cae3a66f5cafed0b08c03c29bae127f7e2181db7
Victor Malyshev <vmalyshev0@gmail.com>
2015-12-30 21:51:46 +0700

    Fixing templates issue with attributes for a select options

--- app/scripts/views/editQuest.js
+++ app/scripts/views/editQuest.js
@@ -2,15 +2,15 @@
 
 TheGame.Views = TheGame.Views || {};
 
-(function () {
+(function() {
 	'use strict';
 
-	TheGame.Views.EditQuestView = Backbone.View.extend({
+	TheGame.Views.EditQuestView = Backbone.View.extend( {
 
-		tagName: 'div',
+		tagName:   'div',
 		className: 'questDetails row col-sm-4',
 
-		template: _.template($('#questDetailsTemplate').html()),
+		template: _.template( $( '#questDetailsTemplate' ).html() ),
 
 		events: {
 			'click #close':      'close',
@@ -19,44 +19,51 @@ TheGame.Views = TheGame.Views || {};
 
 		},
 
-		initialize: function () {
-			this.listenTo(this.model, 'change', this.render);
-			this.listenTo(this.model, 'destroy', this.close);
+		initialize: function() {
+			this.listenTo( this.model, 'change', this.render );
+			this.listenTo( this.model, 'destroy', this.close );
 		},
 
-		render: function () {
-			this.$el.html(this.template(this.model.toJSON()));
+		render: function() {
+
+			this.$el.html( this.template( this.model.toJSON() ) );
+			//Populate priority
+			this.$el.find( 'option' )
+				  .get(Number.parseInt(this.model
+						.get( 'priority' )) ).selected = true;
+
 			return this;
 		},
 
-		parse: function ( response ) {
+		parse: function( response ) {
 			response.id = response._id;
 			return response;
 		},
 
-		close: function () {
+		close: function() {
 			// Delete view
 			this.remove();
 		},
 
-		saveEdits: function () {
+		saveEdits: function() {
 			//Update the model
 			//Get all the data
-			var comment    = $('#qcomments').val(),
-			    attributes = {};
+			var comment = $( '#qcomments' ).val(),
+				  attributes = {};
 
-			attributes[ 'name' ] = $('#qnameEdit').val();
-			attributes[ 'description' ] = $('#qdescriptionEdit').val();
-			attributes[ 'priority' ] = Number.parseInt($('#qpriorityEdit').val()) || 0;
+			attributes['name'] = $( '#qnameEdit' ).val();
+			attributes['description'] = $( '#qdescriptionEdit' ).val();
+			attributes['priority'] = Number.parseInt( $( '#qpriorityEdit' ).val() ) || 0;
 
-			if ( comment !== "" ) {
+			if( comment !== "" ) {
 				//Update in model
-				attributes[ 'comments' ] = comment;
-				attributes[ 'isComment' ] = true;
-			} else {
-				attributes[ 'isComment' ] = false;
+				attributes['comments'] = comment;
+				attributes['isComment'] = true;
 			}
-			this.updateModelData(attributes);
+			else {
+				attributes['isComment'] = false;
+			}
+			this.updateModelData( attributes );
 
 		},
 
@@ -64,17 +71,17 @@ TheGame.Views = TheGame.Views || {};
 			this.saveEdits();
 		},
 
-		updateModelData: function ( values ) {
+		updateModelData: function( values ) {
 			var that = this;
-			Object.keys(values).forEach(function ( key ) {
-				var value = values[ key ];
+			Object.keys( values ).forEach( function( key ) {
+				var value = values[key];
 				// iteration code
-				if ( value !== '' ) that.model.set(key, value);
-			});
+				if( value !== '' ) that.model.set( key, value );
+			} );
 			this.model.save();
 
 		}
 
-	});
+	} );
 
 })();
